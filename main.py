ed681ee29e8af39ee6273c925f8ee125eb5268f4
Marta Bianchini <marta.bianchini24@gmail.com>
2023-04-02 19:19:55 +0100

    implemented simple function, not run yet

---
+++ main.py
@@ -0,0 +1,20 @@
+import pyttsx3  # module - text to speech coversion library
+import PyPDF2  # Library - used to perform major tasks on PDF files
+
+
+def read_pdf(pdf):
+    pdf_reader = PyPDF2.PdfFileReader(open(pdf, 'rb'))
+    speaker = pyttsx3.init()
+
+    for page_num in range(pdf_reader.numPages):
+        text = pdf_reader.getPage.extractText()
+        clean_text = text.strip().replace('\n', ' ')
+        print(clean_text)
+
+    speaker.save_to_file(clean_text, 'pdf_audio.mp3')
+    speaker.runAndWait
+
+    speaker.stop
+
+
+read_pdf('')
